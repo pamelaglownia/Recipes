6c4714db4827c63d8cca46328f6d9e8d9b8d7d5b
Pam <pamela.glownia@gmail.com>
2021-11-30 19:06:52 +0100

    Generate Spring Boot project

---
+++ src/test/java/pl/glownia/pamela/recipes/RecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.glownia.pamela.recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
