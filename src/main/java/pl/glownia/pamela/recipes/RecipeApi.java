41c75c77f5866d64440c44961a120122f2078ce7
Pam <pamela.glownia@gmail.com>
2021-12-04 11:14:08 +0100

    Create HashMap contains recipes

--- src/main/java/pl/glownia/pamela/recipes/RecipeApi.java
+++ src/main/java/pl/glownia/pamela/recipes/RecipeApi.java
@@ -2,24 +2,38 @@ package pl.glownia.pamela.recipes;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
 @RestController
+@RequestMapping("/api/recipes")
 public class RecipeApi {
 
-    private Recipe recipe;
+    private final HashMap<Integer, Recipe> cookbook;
 
     public RecipeApi() {
-        recipe = new Recipe("Hot chocolate", "Hot chocolate for cold winter", "Cocoa powder, milk, honey, chilli flakes",
-                "Place the milk, cocoa powder in a saucepan and heat until warm.Add honey and chilli flakes.");
+        cookbook = new HashMap<>();
+        cookbook.put(cookbook.size() + 1, new Recipe("blabla", "description....", "ingredients...",
+                "directions...."));
+        cookbook.put(cookbook.size() + 1, new Recipe("Hot chocolate", "Hot chocolate for cold winter", "Cocoa powder, milk, honey, chilli",
+                "Place the milk, cocoa powder in a saucepan and heat until warm.Add honey and chilli."));
+        cookbook.put(cookbook.size() + 1, new Recipe("Cheese sandwich with ketchup", "Simple sandwich for student", "bread, butter, cheese, ketchup",
+                "Spread butter on bread, put one slice of cheese and add ketchup on the top."));
     }
 
-    @GetMapping("/api/recipe")
-    public Recipe getRecipe() {
-        return recipe;
+    @GetMapping("/all")
+    public HashMap<Integer, Recipe> getAllRecipes() {
+        return cookbook;
     }
 
-    @PostMapping("/api/recipe")
-    void changeRecipe() {
-        recipe = new Recipe("Cheese sandwich with ketchup", "Simple sandwich for student", "bread, butter, cheese, ketchup",
-                "Spread butter on bread, put one slice of cheese and add ketchup on the top.");
+    @GetMapping
+    public Optional<Recipe> getRecipeById(@RequestParam Integer recipeId) {
+        return cookbook.entrySet().stream().filter(element -> element.getKey().equals(recipeId)).map(Map.Entry::getValue).findFirst();
+    }
+
+    @PostMapping("/add")
+    void addRecipe(@RequestBody Recipe recipe) {
+        cookbook.put(cookbook.size() + 1, recipe);
     }
 }
