be52ecb97bc6e9b58baa0e0c2c2d9571b4cd1be0
Pam <pamela.glownia@gmail.com>
2021-12-05 13:38:45 +0100

    Refactor

--- src/main/java/pl/glownia/pamela/recipes/Recipe.java
+++ src/main/java/pl/glownia/pamela/recipes/Recipe.java
@@ -1,16 +1,19 @@
 package pl.glownia.pamela.recipes;
 
 public class Recipe {
-    private final String name;
-    private final String description;
-    private final String ingredients;
-    private final String directions;
+    private String name;
+    private String description;
+    private String ingredients;
+    private String directions;
 
     public Recipe(String name, String description, String ingredients, String directions) {
         this.name = name;
         this.description = description;
         this.ingredients = ingredients;
         this.directions = directions;
+    }
+
+    public Recipe() {
     }
 
     public String getName() {
@@ -27,5 +30,21 @@ public class Recipe {
 
     public String getDirections() {
         return directions;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public void setDirections(String directions) {
+        this.directions = directions;
     }
 }
--- src/main/java/pl/glownia/pamela/recipes/RecipeApi.java
+++ src/main/java/pl/glownia/pamela/recipes/RecipeApi.java
@@ -1,39 +1,33 @@
 package pl.glownia.pamela.recipes;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/recipes")
 public class RecipeApi {
+    RecipeManager cookbook;
 
-    private final HashMap<Integer, Recipe> cookbook;
-
-    public RecipeApi() {
-        cookbook = new HashMap<>();
-        cookbook.put(cookbook.size() + 1, new Recipe("blabla", "description....", "ingredients...",
-                "directions...."));
-        cookbook.put(cookbook.size() + 1, new Recipe("Hot chocolate", "Hot chocolate for cold winter", "Cocoa powder, milk, honey, chilli",
-                "Place the milk, cocoa powder in a saucepan and heat until warm.Add honey and chilli."));
-        cookbook.put(cookbook.size() + 1, new Recipe("Cheese sandwich with ketchup", "Simple sandwich for student", "bread, butter, cheese, ketchup",
-                "Spread butter on bread, put one slice of cheese and add ketchup on the top."));
+    @Autowired
+    public RecipeApi(RecipeManager cookbook) {
+        this.cookbook = cookbook;
     }
 
     @GetMapping("/all")
     public HashMap<Integer, Recipe> getAllRecipes() {
-        return cookbook;
+        return cookbook.getCookbook();
     }
 
-    @GetMapping
-    public Optional<Recipe> getRecipeById(@RequestParam Integer recipeId) {
-        return cookbook.entrySet().stream().filter(element -> element.getKey().equals(recipeId)).map(Map.Entry::getValue).findFirst();
+    @GetMapping("/{recipeId}")
+    public Optional<Recipe> getRecipeById(@PathVariable Integer recipeId) {
+        return cookbook.getChosenRecipe(recipeId);
     }
 
     @PostMapping("/add")
     void addRecipe(@RequestBody Recipe recipe) {
-        cookbook.put(cookbook.size() + 1, recipe);
+        cookbook.addRecipe(recipe);
     }
 }
