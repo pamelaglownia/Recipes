fc57be0e1bee482c0c8376195f12b975f63c4691
Pam <pamela.glownia@gmail.com>
2021-12-21 20:30:01 +0100

    Use lombok in model class

--- src/main/java/pl/glownia/pamela/recipes/model/Recipe.java
+++ src/main/java/pl/glownia/pamela/recipes/model/Recipe.java
@@ -1,13 +1,15 @@
 package pl.glownia.pamela.recipes.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
 public class Recipe {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -39,62 +41,6 @@ public class Recipe {
         this.date = LocalDateTime.now();
         this.description = description;
         this.ingredients = ingredients;
-        this.directions = directions;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getIngredients() {
-        return ingredients;
-    }
-
-    public void setIngredients(String ingredients) {
-        this.ingredients = ingredients;
-    }
-
-    public String getDirections() {
-        return directions;
-    }
-
-    public void setDirections(String directions) {
         this.directions = directions;
     }
 }
