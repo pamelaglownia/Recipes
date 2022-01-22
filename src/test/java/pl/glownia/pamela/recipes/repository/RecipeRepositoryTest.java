b02e08064a70a928de01990d7e1b93afefd8222c
pamela.glownia <pamela.glownia@gmail.com>
2022-01-22 15:14:36 +0100

    Test getting all user's recipes

--- src/test/java/pl/glownia/pamela/recipes/repository/RecipeRepositoryTest.java
+++ src/test/java/pl/glownia/pamela/recipes/repository/RecipeRepositoryTest.java
@@ -8,18 +8,20 @@ import pl.glownia.pamela.recipes.model.Recipe;
 import pl.glownia.pamela.recipes.model.User;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DataJpaTest
-class RecipeRepositoryTest {
+public class RecipeRepositoryTest {
 
     @Autowired
     RecipeRepository underTestRepository;
 
     @AfterEach
-    void tearDown() {
+    public void tearDown() {
         underTestRepository.deleteAll();
     }
 
@@ -74,7 +76,7 @@ class RecipeRepositoryTest {
     }
 
     @Test
-    void itShouldCheckIfRecipeFromChosenCategoryExists() {
+    public void itShouldCheckIfRecipeFromChosenCategoryExists() {
         //given
         String recipeCategory = "beverage";
         User julia = User.builder()
@@ -123,5 +125,42 @@ class RecipeRepositoryTest {
 
         //then
         assertThat(expected).isNotEqualTo(cookbook);
+    }
+
+    @Test
+    void itShouldGetAllUserRecipes() {
+        //given
+        User julia = User.builder()
+                .email("julia@gmail.com")
+                .password("pass123!")
+                .role("USER")
+                .build();
+        Recipe winterCoffee = Recipe.builder()
+                .name("Winter coffee")
+                .category("beverage")
+                .description("Simple coffee for cold winter")
+                .ingredients("coffee beans, , milk, cinnamon, honey, star anise, cloves")
+                .directions("Prepare coffee in your favourite cup, add milk, honey and all spices.")
+                .user(julia)
+                .build();
+        underTestRepository.save(winterCoffee);
+        Recipe iceCoffee = Recipe.builder()
+                .name("Ice coffee")
+                .category("beverage")
+                .description("Simple coffee for sunny day")
+                .ingredients("coffee beans, , milk, sugar, ice cubes")
+                .directions("Prepare coffee with sugar in tall glass, add few ice cubes and milk.")
+                .user(julia)
+                .build();
+        underTestRepository.save(iceCoffee);
+        Set<Recipe> recipes = new HashSet<>();
+
+        recipes.add(winterCoffee);
+        recipes.add(iceCoffee);
+        //when
+        Set<Recipe> expected = underTestRepository.findByUser(julia);
+
+        //then
+        assertThat(expected).isEqualTo(recipes);
     }
 }
